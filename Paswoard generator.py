c64fa47835b1dfd2fc32b3887e054d2ed987f182
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-07-20 16:38:14 +0530

    Delete Paswoard generator.py

--- Paswoard generator.py
+++
@@ -1,9 +0,0 @@
-import random
-lower="abcdegghijklmnopqrstuvwxyz"
-upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-number="0123456789"
-charecters="<>!@$%^&*+"
-string=lower+upper+number+charecters
-length=9
-pasword="".join(random.sample(string,length))
-print("Your password is: ",pasword)
